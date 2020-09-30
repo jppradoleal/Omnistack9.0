34be7eb0cc8eb862d7c9ecdab5bbf8adc3c2abb2
Joao Pedro Prado Leal <joaopedro0128@hotmail.com>
2020-09-30 00:09:30 -0300

    Made changes to accept cors and send the url for images

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
+const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const routes = require('./routes');
 
@@ -7,6 +9,8 @@ require('dotenv').config();
 
 const app = express();
 
+app.use(cors());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(express.json());
 app.use(routes);
 
