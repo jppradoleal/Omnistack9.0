51a1cfd69ce089d7a0f1a87c9969171cf61faa64
Joao Pedro Prado Leal <joaopedro0128@hotmail.com>
2020-10-10 19:06:02 -0300

    added spots listing - added booking logic

--- mobile/src/pages/Book.js
+++ mobile/src/pages/Book.js
@@ -1,8 +1,93 @@
-import React from 'react';
-import { View } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import React, { useState } from 'react';
+import { SafeAreaView, Text, TextInput, TouchableOpacity, StyleSheet, Platform, Alert } from 'react-native';
 
-export default function Book() {
+import api from '../services/api';
+
+export default function Book({ navigation }) {
+    const [date, setDate] = useState('');
+
+    const id = navigation.getParam('id');
+
+    async function handleSubmit() {
+        const user_id = await AsyncStorage.getItem('user');
+
+        await api.post(`/spots/${id}/bookings`, {
+            date
+        }, {
+            headers: { user_id }
+        });
+
+        Alert.alert('Solicitação de reserva enviada.');
+        navigation.navigate('List');
+    }
+
+    function handleCancel() {
+        navigation.navigate('List');
+    }
+
     return (
-        <View />
-    )
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.label}>SUA DATA DE INTERESSE *</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Qual data você quer reservar?"
+                placeholderTextColor="#999"
+                autoCapitalize="words"
+                autoCorrect={false}
+                value={date}
+                onChangeText={setDate}
+            />
+            <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+                <Text style={styles.buttonText}>Encontrar spots</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.button, styles.cancelButton]} onPress={handleCancel}>
+                <Text style={styles.buttonText}>Cancelar</Text>
+            </TouchableOpacity>
+        </SafeAreaView> 
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: Platform.OS === 'android' ? 50 : 0,
+        margin: 30
+    },
+
+    label: {
+        fontWeight: 'bold',
+        color: '#444',
+        marginBottom: 8,
+        marginTop: 30,
+    },
+
+    input: {
+        borderWidth: 1,
+        borderColor: "#ddd",
+        paddingHorizontal: 20,
+        fontSize: 16,
+        color: '#444',
+        height: 44,
+        marginBottom: 20,
+        borderRadius: 2,
+    },
+
+    button: {
+        height: 32,
+        backgroundColor: '#f05a5b',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 2,
+    },
+
+    cancelButton: {
+        marginTop: 10,
+        backgroundColor: '#ccc',
+    },
+
+    buttonText: { 
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 15,
+    }
+})
--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,8 +1,43 @@
-import React from 'react';
-import { View } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, ScrollView, Text, Image, StyleSheet, Platform } from 'react-native';
+
+import SpotList from '../Components/SpotList';
+
+import logo from '../assets/logo.png'
 
 export default function List() {
+    const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storagedTechs => {
+            const techsArray = storagedTechs
+                                .split(',')
+                                .map(tech=> tech.trim());
+            setTechs(techsArray);
+        })
+    }, [])
+
     return (
-        <View />
+        <SafeAreaView style={styles.container}>
+            <Image source={logo} style={styles.logo}/>
+            <ScrollView>
+                {techs.map(tech => <SpotList tech={tech} key={tech}/>)}
+            </ScrollView>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: Platform.OS === 'android' ? 50 : 0
+    },
+
+    logo: {
+        height: 32,
+        resizeMode: 'contain',
+        alignSelf: 'center',
+        marginTop: 10,
+    }
+});
