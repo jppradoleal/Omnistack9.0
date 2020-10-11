3b7d9181423148d8fdffb5158dd6879c7c8a1145
Joao Pedro Prado Leal <joaopedro0128@hotmail.com>
2020-10-11 18:17:09 -0300

    added date, user and owner validation for booking controller

--- backend/src/controllers/BookingController.js
+++ backend/src/controllers/BookingController.js
@@ -1,4 +1,6 @@
 const Booking = require('../models/Booking');
+const Spot = require('../models/Spot');
+const User = require('../models/User');
 
 module.exports = {
     async store(req, res) {
@@ -6,22 +8,41 @@ module.exports = {
         const { spot_id } = req.params;
         const { date } = req.body;
 
-        const booking = await Booking.create({
-            user: user_id,
-            spot: spot_id,
-            date,
-        });
+        let message = '';
 
-        await booking.populate('spot').populate('user').execPopulate();
+        message = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/
+                    .test(date) ? '' : 'Date must be on DD-MM-YYYY or DD/MM/YYYY';
 
-        
-        const ownerSocket = req.connectedUsers[booking.spot.user];
-        
-        if(ownerSocket) {
-            console.log(req.connectedUsers[booking.spot.user])
-            req.io.to(ownerSocket).emit('booking_request', booking);
+        const spot_owner = (await Spot.findById(spot_id)).user;
+        const user = await User.findById(user_id);
+        if(user && !message.length) {
+            if(user_id != spot_owner) {
+                
+                const booking = await Booking.create({
+                    user: user_id,
+                    spot: spot_id,
+                    date,
+                });
+                
+                await booking.populate('spot').populate('user').execPopulate();
+                
+                const ownerSocket = req.connectedUsers[booking.spot.user];
+                
+                if(ownerSocket) {
+                    console.log(req.connectedUsers[booking.spot.user])
+                    req.io.to(ownerSocket).emit('booking_request', booking);
+                }
+                
+                return res.status(202).json(booking);
+            } else {
+                message = 'Requester cannot be company owner.';
+            }
+        } else {
+            message = message || 'User not found';
         }
-
-        return res.status(202).json(booking);
+            
+        return res.status(400).json({
+            message
+        });
     }
 }
